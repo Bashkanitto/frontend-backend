7a09412af9eeb43ed3ddcf07e822742c6d7ff262
aidyn <aidyn.meiramkhanuly@gmail.com>
2025-09-19 22:40:32 +0500

    merged conflics

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import './App.css'
-import Footer from './components/Footer/Footer'
-import Banner from './components/Banner/Banner'
-
-
+import './App.css';
+import Footer from './components/Footer/Footer';
+import Banner from './components/Banner/Banner';
+import Header from './components/Header/Header';
 
 // App.tsx
 function App() {
   return (
     <>
-      <Banner 
+      <Header />
+      <Banner
         title="typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
         subtitle="bbb"
         buttonText="View"
@@ -18,8 +17,7 @@ function App() {
       />
       <Footer />
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
