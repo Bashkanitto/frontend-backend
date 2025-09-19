cec92c4f495ccc882fb7fc1245413fd597c2f57d
Grisha <griiisha2111@gmail.com>
2025-09-19 22:36:23 +0500

    feat: added banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 import Footer from './components/Footer/Footer'
+import Banner from './components/Banner/Banner'
 
+
+
+// App.tsx
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <Footer />
+    <>
+      <Banner 
+        title="typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        subtitle="bbb"
+        buttonText="View"
+        leftIcon="/images/gamepad.svg"
+        rightImage="/images/minecraft.png"
+      />
+      <Footer />
+    </>
   )
 }
+
 
 export default App
---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,41 @@
+import arrow from '../../../public/images/arrow-right.svg'
+import styles from './Banner.module.css'
+
+type BannerProps = {
+  title: string
+  subtitle: string
+  buttonText: string
+  leftIcon?: string
+  rightImage?: string
+}
+
+
+function Banner({ title, subtitle, buttonText, leftIcon, rightImage }: BannerProps) {
+  return (
+    <div className={styles.paddingGlobal}>
+      <div className={styles.banner}>
+        <div className={styles.bannerLeft}>
+          {leftIcon && (
+            <div className={styles.whiteCircle}>
+              <img src={leftIcon} alt="icon" />
+            </div>
+          )}
+          <h1>{title}</h1>
+          <p>{subtitle}</p>
+          <button className={styles.bannerButton}>
+            <p className={styles.bannerButtonText}>{buttonText}</p>
+            <div className={`${styles.whiteCircle} ${styles.whiteCircleSmall}`}>
+              <img src={arrow} />
+            </div>
+          </button>
+        </div>
+
+        <div className={styles.bannerRight}>
+          {rightImage && <img src={rightImage} alt="banner" />}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
